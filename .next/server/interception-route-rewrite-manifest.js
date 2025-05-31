48402fa128897154b94ab6ba84ac66fd65cb2811
ronetel <144216054+ronetel@users.noreply.github.com>
2025-05-31 19:09:34 +0300

    Delete .next directory

--- .next/server/interception-route-rewrite-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__INTERCEPTION_ROUTE_REWRITE_MANIFEST="[{\"source\":\"/product/:id\",\"destination\":\"/(.)product/:id\",\"has\":[{\"type\":\"header\",\"key\":\"Next-Url\",\"value\":\"(?:\\\\/(.*))?[\\\\/#\\\\?]?\"}]}]"
